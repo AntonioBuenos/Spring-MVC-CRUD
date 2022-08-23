221938254d08a7759804bf4008cf2de6fd64d462
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-08-23 12:10:39 +0300

    DAO

---
+++ src/main/java/by/smirnov/springtest/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package by.smirnov.springtest.dao;
+
+import by.smirnov.springtest.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Bruce"));
+        people.add(new Person(++PEOPLE_COUNT, "Paul"));
+        people.add(new Person(++PEOPLE_COUNT, "Blaze"));
+        people.add(new Person(++PEOPLE_COUNT, "Adrian"));
+        people.add(new Person(++PEOPLE_COUNT, "Steve"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return (Person) people.stream().filter(person -> person.getId()==id).findAny().orElse(null);
+    }
+}
---
+++ src/main/java/by/smirnov/springtest/models/Person.java
@@ -0,0 +1,17 @@
+package by.smirnov.springtest.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Person {
+
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
