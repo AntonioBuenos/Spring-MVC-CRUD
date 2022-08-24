2e60473c8a3c210b08e40a16e32e746392302aaf
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-08-24 12:45:08 +0300

    @Valid

--- src/main/java/by/smirnov/springtest/dao/PersonDAO.java
+++ src/main/java/by/smirnov/springtest/dao/PersonDAO.java
@@ -13,11 +13,11 @@ public class PersonDAO {
 
     {
         people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Bruce"));
-        people.add(new Person(++PEOPLE_COUNT, "Paul"));
-        people.add(new Person(++PEOPLE_COUNT, "Blaze"));
-        people.add(new Person(++PEOPLE_COUNT, "Adrian"));
-        people.add(new Person(++PEOPLE_COUNT, "Steve"));
+        people.add(new Person(++PEOPLE_COUNT, "Bruce", 64, "bruce@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Paul", 64, "paul@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Blaze", 59, "blaze@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Adrian", 65, "adrian@gmail.com"));
+        people.add(new Person(++PEOPLE_COUNT, "Steve", 66, "steve@gmail.com"));
     }
 
     public List<Person> index() {
@@ -37,6 +37,8 @@ public class PersonDAO {
         Person personToBeUpdated = show(id);
 
         personToBeUpdated.setName(updatedPerson.getName());
+        personToBeUpdated.setAge(updatedPerson.getAge());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());
     }
 
     public void delete(int id) {
--- src/main/java/by/smirnov/springtest/models/Person.java
+++ src/main/java/by/smirnov/springtest/models/Person.java
@@ -2,19 +2,33 @@ package by.smirnov.springtest.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class Person {
-
     private int id;
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should consist of 2 up to 30 characters")
     private String name;
+    @Min(value = 0, message = "Age cannot be negaive")
+    private int age;
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    private String email;
 
     public Person() {
     }
 
-    public Person(int id, String name) {
+    public Person(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
+        this.age = age;
+        this.email = email;
     }
 }
