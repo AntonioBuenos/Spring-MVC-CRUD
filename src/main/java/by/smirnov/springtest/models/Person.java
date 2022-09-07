cc450beedd2c153d2bd13e8d1d45e5d5b9d1e4de
AntonioBuenos <104083531+AntonioBuenos@users.noreply.github.com>
2022-09-07 09:31:22 +0300

    Person address pattern validator added.

--- src/main/java/by/smirnov/springtest/models/Person.java
+++ src/main/java/by/smirnov/springtest/models/Person.java
@@ -1,34 +1,33 @@
 package by.smirnov.springtest.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
 
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     private int id;
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should consist of 2 up to 30 characters")
     private String name;
-    @Min(value = 0, message = "Age cannot be negaive")
+    @Min(value = 0, message = "Age cannot be negative")
     private int age;
     @NotEmpty(message = "Email should not be empty")
     @Email(message = "Email should be valid")
     private String email;
+    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}",
+            message = "Your address shall conform the format: Country, City, Postal code (6 digits)")
+    private String address;
 
-    public Person() {
-    }
-
-    public Person(int id, String name, int age, String email) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.email = email;
-    }
 }
